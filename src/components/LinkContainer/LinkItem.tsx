dc28f59b4f540688ad5d44b60fd23144b05379ad
Artem Kaloshin <a.i.kaloshin@gmail.com>
2023-08-01 14:06:21 +0300

    Update github action

--- src/components/LinkContainer/LinkItem.tsx
+++ src/components/LinkContainer/LinkItem.tsx
@@ -6,7 +6,7 @@ function LinkItem(props: {link: any}) {
 
     return (
         <div className={styles.item}>
-            <a href={link.link} target='_blank' className={styles.itemLink}>
+            <a href={link.link} target='_blank' rel='noreferrer' className={styles.itemLink}>
                 <img src={link.logo} alt={link.title}/>
                 <span>{link.title}</span>
             </a>
